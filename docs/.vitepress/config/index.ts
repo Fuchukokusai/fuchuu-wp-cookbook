19a4f58fbc8a88cf131acf4c5888f72b3e0cb1e4
suhanyujie <suhanyujie@qq.com>
2025-04-11 21:45:03 +0900

    调整打包是的 base 配置

--- docs/.vitepress/config/index.ts
+++ docs/.vitepress/config/index.ts
@@ -5,7 +5,8 @@ import { jp } from './jp';
 export default defineConfig({
   // 站点级选项
   title: '府中国際サロン',
-  description: 'Just playing around.',
+  description: '府中国際サロンのホームページ管理のマニュアル',
+  base: '/t1/script/dist/dist/',
 
   locales: {
     root: {
