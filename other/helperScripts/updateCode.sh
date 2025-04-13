0a9da0343bed2a3af49b2ed443e59743b9066744
suhanyujie <suhanyujie@qq.com>
2025-04-13 21:22:09 +0900

    add shell for update code

---
+++ other/helperScripts/updateCode.sh
@@ -0,0 +1,13 @@
+#!/usr/bin
+#
+# test env
+cd ~/web/test/wp-content/themes/devotion-pro/ && git pull;
+cd ~/web/test/wp-content/plugins/wp-link-gallery/ && git pull;
+
+# pro env
+cd ~/web/fuchukokusai.com/wp-content/themes/devotion-pro/ && git pull;
+cd ~/web/fuchukokusai.com/wp-content/plugins/wp-link-gallery/ && git pull;
+
+
+
+
