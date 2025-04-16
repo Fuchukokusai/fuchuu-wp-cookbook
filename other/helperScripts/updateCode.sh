016c1f28fdbfa630c6accda5c107a9381f61f522
suhanyujie <suhanyujie@qq.com>
2025-04-16 20:28:39 +0900

    update script for file sync

--- other/helperScripts/updateCode.sh
+++ other/helperScripts/updateCode.sh
@@ -9,5 +9,6 @@ cd ~/web/fuchukokusai.com/wp-content/themes/devotion-pro/ && git pull;
 cd ~/web/fuchukokusai.com/wp-content/plugins/wp-link-gallery/ && git pull;
 
 
-
+# file upload cmd: rsync -Pcauv source/ target/
+# rsync -Pcauv ~/web/fuchukokusai.com/wp-content/uploads/2025/04 ~/web/test/wp-content/uploads/2025/04
 
